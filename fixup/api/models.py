4b791f1c79e7f3c34df0d3f5a776d9fa9c065818
Navanith Niranjan <navanith.niran@gmail.com>
2023-07-24 17:45:14 -0400

    changed to mysql database

--- fixup/api/models.py
+++ fixup/api/models.py
@@ -3,35 +3,39 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    SEX_OPTIONS = [
-        ("M", "Male"),
-        ("F", "Female"),
-    ]
+    pass
+    # SEX_OPTIONS = [
+    #     ("M", "Male"),
+    #     ("F", "Female"),
+    # ]
 
-    age = models.IntegerField(default=0)
-    sex = models.CharField(max_length=1, choices=SEX_OPTIONS)
-    height = models.CharField(max_length=3)
-    weight = models.CharField(max_length=3)
+    # age = models.IntegerField(default=0)
+    # sex = models.CharField(max_length=1, choices=SEX_OPTIONS)
+    # height = models.CharField(max_length=3)
+    # weight = models.CharField(max_length=3)
 
 class WorkoutHistory(models.Model):
-    user = models.ForeignKey()
-    session_name = models.CharField()
-    date = models.DateField()
+    pass
+    # user = models.ForeignKey()
+    # session_name = models.CharField()
+    # date = models.DateField()
 
 class Sessions(models.Model):
-    session = models.ForeignKey()
-    exercise = models.ForeignKey()
-    sets = models.IntegerField()
-    weight = models.FloatField()
-    kgorlb = models.CharField()
-    reps = models.IntegerField()
-    time = models.TimeField()
-    pace = models.TimeField()
-    distance = models.FloatField()
-    kmormiles = models.CharField()
-    notes = models.CharField()
-    session_duration = models.TimeField()
+    pass
+    # session = models.ForeignKey()
+    # exercise = models.ForeignKey()
+    # sets = models.IntegerField()
+    # weight = models.FloatField()
+    # kgorlb = models.CharField()
+    # reps = models.IntegerField()
+    # time = models.TimeField()
+    # pace = models.TimeField()
+    # distance = models.FloatField()
+    # kmormiles = models.CharField()
+    # notes = models.CharField()
+    # session_duration = models.TimeField()
 
 class ExerciseList(models.Model):
-    exercise_name = models.CharField()
-    exercise_type = models.CharField()
+    pass
+    # exercise_name = models.CharField()
+    # exercise_type = models.CharField()
