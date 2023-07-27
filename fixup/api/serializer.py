2d2fda61b7c7e744574e53b4dd68f1dc0b446948
Navanith Niranjan <navanith.niran@gmail.com>
2023-07-27 18:17:36 -0400

    added more serializers

--- fixup/api/serializer.py
+++ fixup/api/serializer.py
@@ -1,5 +1,13 @@
-from .models import User
+from .models import User, WorkoutHistory, Sessions, ExerciseList
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
+    pass
+
+class WorkoutHistorySerializer(serializers.ModelSerializer):
+    pass
+
+class SessionsSerializer(serializers.ModelSerializer):
+    pass
+class ExerciseListSerializer(serializers.ModelSerializer):
     pass
