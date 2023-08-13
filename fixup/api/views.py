1be7e6ab07750707453098531282761a97a6b388
Navanith Niranjan <navanith.niran@gmail.com>
2023-08-13 12:44:39 -0400

    modified views

--- fixup/api/views.py
+++ fixup/api/views.py
@@ -1,5 +1,19 @@
-from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from rest_framework.response import Response
+from .models import User
+from .serializers import UserSerializer
 
 # Create your views here.
-def index(request):
-    return HttpResponse("wolf")
+class UserListView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
