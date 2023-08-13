9db27fba8fd43f10c90539fec0c496ee28dfa8b7
Sajeiv Ravichandran <108480967+Sajeiv@users.noreply.github.com>
2023-08-13 13:07:54 -0400

    Added update and delete mixin

--- fixup/api/views.py
+++ fixup/api/views.py
@@ -1,10 +1,10 @@
 from rest_framework.views import APIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer
 
-class UserListView(ListModelMixin, CreateModelMixin, APIView):
+class UserListView(ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, APIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
@@ -13,3 +13,27 @@ class UserListView(ListModelMixin, CreateModelMixin, APIView):
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, *args, **kwargs):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, pk, *args, **kwargs):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, *args, **kwargs):
+        user = self.get_object(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
