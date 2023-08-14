8176029264f608840fdbb1bad18110458a5da4da
Navanith Niranjan <navanith.niran@gmail.com>
2023-08-14 08:54:06 -0400

    modified urls.py to include more api endpoints

--- fixup/api/urls.py
+++ fixup/api/urls.py
@@ -5,9 +5,9 @@ from .views import WorkoutHistoryViewSet, SessionsViewSet
 
 router = DefaultRouter()
 router.register(r'users', views.UserViewSet, basename='user')
-router.register(r'workout-history', WorkoutHistoryViewSet)
-router.register(r'sessions', SessionsViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api/users/<int:user_pk>/workout-history/', views.WorkoutHistoryViewSet.as_view({'get': 'list', 'post': 'create'}), name='user-workout-history-list'),
+    path('api/users/<int:user_pk>/workout-history/<int:pk>/', views.SessionsViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='user-sessions-detail'),
 ]
