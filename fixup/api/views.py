e4d1e47d46ec5a87b1336159b5c44106e95678b2
Navanith Niranjan <navanith.niran@gmail.com>
2023-08-16 15:28:24 -0400

    fixed the issue regarding saving workout history to a specific user

--- fixup/api/views.py
+++ fixup/api/views.py
@@ -66,6 +66,11 @@ class WorkoutHistoryViewSet(viewsets.ModelViewSet):
     serializer_class = WorkoutHistorySerializer
 
     def list(self, request, pk=None):
+        try:
+            user = User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
         workout_history_entries = self.queryset.filter(user=pk)
         serializer = self.serializer_class(workout_history_entries, many=True)
         return Response(serializer.data)
@@ -76,9 +81,9 @@ class WorkoutHistoryViewSet(viewsets.ModelViewSet):
         except User.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
-        serializer = self.serializer_class(user, data=request.data)
+        serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
-            serializer.save() 
+            serializer.save(user=user) 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
