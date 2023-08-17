3b153575c1509e2d7a5b9149923ebcf7d2441770
Navanith Niranjan <navanith.niran@gmail.com>
2023-08-17 14:51:27 -0400

    Changed models.py by splitting Sessions model into seperate tables based on type of exercise

--- fixup/api/models.py
+++ fixup/api/models.py
@@ -20,26 +20,45 @@ class WorkoutHistory(models.Model):
     user = models.ForeignKey('User', on_delete=models.CASCADE, null=True)
     session_name = models.CharField(null=True, max_length=100)
     date = models.DateField(default=datetime.date.today)
-
     session_number = models.PositiveIntegerField(null=True)
+    session_duration = models.TimeField(null=True)
 
 class Sessions(models.Model):
     workout_history = models.ForeignKey('WorkoutHistory', on_delete=models.CASCADE, null=True)
     exercise = models.ForeignKey('ExerciseList', on_delete=models.CASCADE, blank=True, null=True)
     custom_exercise = models.ForeignKey('CustomExerciseList', on_delete=models.CASCADE, blank=True, null=True)
     exercise_number = models.IntegerField(null=True)
-    sets = models.IntegerField(null=True)
-    set_number = models.IntegerField(null=True)
+    notes = models.CharField(null=True, max_length=500)
+
+# Types of Exercises
+class WeightLiftSession(models.Model):
+    session = models.ForeignKey('Sessions', on_delete=models.CASCADE, null=True)
+    
+    WEIGHT_METRIC_OPTIONS = [
+        ("kg", "kilograms"),
+        ("lb", "pounds")
+    ]
+
+    set_number = models.IntegerField(null=True, unique=True)
     weight = models.FloatField(null=True)
-    kgorlb = models.CharField(null=True, max_length=3)  # Assuming it's either "kg" or "lb"
+    kgorlb = models.CharField(null=True, max_length=2, choices=WEIGHT_METRIC_OPTIONS)
     reps = models.IntegerField(null=True)
-    time = models.TimeField(null=True)
+    inbetween_rest_time = models.TimeField(null=True)
+
+class RunningSession(models.Model):
+    session = models.ForeignKey('Sessions', on_delete=models.CASCADE, null=True)
+    
+    DISTANCE_METRIC_OPTIONS = [
+        ("km", "kilometers"),
+        ("mi.", "miles")
+    ]
+        
     pace = models.TimeField(null=True)
     distance = models.FloatField(null=True)
-    kmormiles = models.CharField(null=True, max_length=6)  # Assuming it's either "kilometers" or "miles"
-    notes = models.CharField(null=True, max_length=500)
-    session_duration = models.TimeField(null=True)
+    kmormiles = models.CharField(null=True, max_length=3, choices=DISTANCE_METRIC_OPTIONS)
+    time = models.TimeField(null=True)
 
+# Curated Exercise Lists
 class ExerciseList(models.Model):
     exercise_name = models.CharField(null=True, max_length=100)
     exercise_type = models.CharField(null=True, max_length=100)
