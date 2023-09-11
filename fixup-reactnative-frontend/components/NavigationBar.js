9731f071a385d751a71096a1719c36db7463eaf9
Sajeiv <108480967+Sajeiv@users.noreply.github.com>
2023-09-10 22:08:29 -0400

    Added a NavigationBar.js component file which will be included in all main screens of the app, this bar has the buttons to lead to important pages such as workout-history, start workout and profile. More buttons can be added later accordingly

---
+++ fixup-reactnative-frontend/components/NavigationBar.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+const NavigationBar = ({ activeTab, onTabPress }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={[
+          styles.tab,
+          activeTab === 'Home' ? styles.activeTab : null,
+        ]}
+        onPress={() => onTabPress('Home')}
+      >
+        <Text
+          style={[
+            styles.tabText,
+            activeTab === 'Home' ? styles.activeTabText : null,
+          ]}
+        >
+          Home
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[
+          styles.tab,
+          activeTab === 'Workouts' ? styles.activeTab : null,
+        ]}
+        onPress={() => onTabPress('Workouts')}
+      >
+        <Text
+          style={[
+            styles.tabText,
+            activeTab === 'Workouts' ? styles.activeTabText : null,
+          ]}
+        >
+          Workouts
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[
+          styles.tab,
+          activeTab === 'Profile' ? styles.activeTab : null,
+        ]}
+        onPress={() => onTabPress('Profile')}
+      >
+        <Text
+          style={[
+            styles.tabText,
+            activeTab === 'Profile' ? styles.activeTabText : null,
+          ]}
+        >
+          Profile
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: 'white', // Background color
+    borderTopWidth: 1,
+    borderTopColor: 'lightgray', // Tab separator color
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 60, // Adjust this value to make the tabs taller
+  },
+  tab: {
+    flex: 1,
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  tabText: {
+    color: 'gray', // Inactive tab text color
+    fontSize: 14, // Adjust font size
+  },
+  activeTab: {
+    backgroundColor: 'white', // Active tab background color
+  },
+  activeTabText: {
+    color: 'black', // Active tab text color
+    fontWeight: 'bold', // Bold text for the active tab
+  },
+});
+
+export default NavigationBar;
